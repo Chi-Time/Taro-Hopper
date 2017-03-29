6821fd0afd18882e1e1e0a78f91275193d92385a
James Johnson <music1384@yahoo.co.uk>
2017-03-29 03:01:39 +0100

    Implemented pad generator behaviour.

--- Assets/Code/Classes/PadGenerator.cs
+++ Assets/Code/Classes/PadGenerator.cs
@@ -1,5 +1,48 @@
 ﻿using UnityEngine;
+using System.Collections;
 
-public class PadGenerator : MonoBehaviour 
+public class PadGenerator : MonoBehaviour
 {
+    [SerializeField] private float _MinDelay = 0f, _MaxDelay = 0f;
+    [SerializeField] private float _MinRange = 0f, _MaxRange = 0f;
+    [SerializeField] private PadPool _Pool = new PadPool ();
+
+    private void Awake ()
+    {
+        Initialise ();
+    }
+
+    private void Initialise ()
+    {
+        _Pool.Initialise ();
+    }
+
+    private void Start ()
+    {
+        SetDefaults ();
+    }
+
+    private void SetDefaults ()
+    {
+        float delay = Random.Range (_MinDelay, _MaxDelay);
+        StartCoroutine (SpawnPad (delay));
+    }
+
+    private IEnumerator SpawnPad (float delay)
+    {
+        yield return new WaitForSeconds (delay);
+
+        var pad = _Pool.RetrieveFromPool ();
+
+        if(pad != null)
+        {
+            float pos = Random.Range (_MinRange, _MaxRange);
+            pad.transform.position = new Vector3 (pos, 15f, 0f);
+        }
+
+        float newDelay = Random.Range (_MinDelay, _MaxDelay);
+
+        StopCoroutine (SpawnPad (newDelay));
+        StartCoroutine (SpawnPad (newDelay));
+    }
 }
