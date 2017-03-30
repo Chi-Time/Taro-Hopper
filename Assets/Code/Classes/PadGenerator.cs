2f6bdbd00492de6e38df6b2fea3b7a4bb5ab4cdd
James Johnson <music1384@yahoo.co.uk>
2017-03-30 02:19:34 +0100

    Fixed random generation code, still needs work.

--- Assets/Code/Classes/PadGenerator.cs
+++ Assets/Code/Classes/PadGenerator.cs
@@ -5,7 +5,12 @@ public class PadGenerator : MonoBehaviour
 {
     [SerializeField] private float _MinDelay = 0f, _MaxDelay = 0f;
     [SerializeField] private float _MinRange = 0f, _MaxRange = 0f;
+    [SerializeField] private float _MinHeight = 0f, _MaxHeight = 0f;
+    [SerializeField] private float _MinWidth = 0f, _MaxWidth = 0f;
     [SerializeField] private PadPool _Pool = new PadPool ();
+
+    private Transform _Player = null;
+    private Transform _LastPad = null;
 
     private void Awake ()
     {
@@ -15,6 +20,7 @@ public class PadGenerator : MonoBehaviour
     private void Initialise ()
     {
         _Pool.Initialise ();
+        _Player = GameObject.FindGameObjectWithTag ("Player").transform;
     }
 
     private void Start ()
@@ -37,7 +43,13 @@ public class PadGenerator : MonoBehaviour
         if(pad != null)
         {
             float pos = Random.Range (_MinRange, _MaxRange);
-            pad.transform.position = new Vector3 (pos, 15f, 0f);
+            float rndH = Random.Range (_MinHeight, _MaxHeight);
+
+            if(_LastPad != null)
+            else
+                pad.transform.position = new Vector3 (pos, 10f, 0f);
+
+            _LastPad = pad.transform;
         }
 
         float newDelay = Random.Range (_MinDelay, _MaxDelay);
