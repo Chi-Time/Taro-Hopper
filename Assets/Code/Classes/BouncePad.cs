ddbfbf4cdd48f22ca187c23e3fd147b5d2bca5fe
James Johnson <music1384@yahoo.co.uk>
2017-03-30 02:49:30 +0100

    Added in fade functionality to bounce pad.

--- Assets/Code/Classes/BouncePad.cs
+++ Assets/Code/Classes/BouncePad.cs
@@ -1,25 +1,33 @@
 ﻿using UnityEngine;
+using System.Collections;
 
 public class BouncePad : MonoBehaviour 
 {
 	[SerializeField] private float _Speed = 0.0f;
+	[SerializeField] private float _FadeTime = 1.0f;
 	[SerializeField] private float _CullBoundary = 0.0f;
 	
 	private PadPool _Pool = null;
+	private Transform _Player = null;
 	private Transform _Transform = null;
 	private Rigidbody2D _Rigidbody2D = null;
+	private SpriteRenderer _SpriteRenderer = null;
 
-	public void Initialise (PadPool pool)
+	private void Awake ()
 	{
 		_Transform = GetComponent<Transform> ();
 		_Rigidbody2D = GetComponent<Rigidbody2D> ();
+		_SpriteRenderer = GetComponent<SpriteRenderer> ();
+	}
 
+	public void Initialise (PadPool pool)
+	{
 		_Pool = pool;
 	}
 
 	private void Start ()
 	{
-		SetDefaults();
+		SetDefaults ();
 	}
 
 	private void SetDefaults ()
@@ -27,12 +35,46 @@ public class BouncePad : MonoBehaviour
 		_Rigidbody2D.gravityScale = 0.0f;
 		_Rigidbody2D.isKinematic = true;
 		_Rigidbody2D.constraints = RigidbodyConstraints2D.FreezeRotation;
+
+		_Player = GameObject.FindGameObjectWithTag ("Player").transform;
+	}
+
+	private void OnEnable ()
+	{
+		StartCoroutine (FadeTo (1.0f, _FadeTime));
+	}
+
+	private IEnumerator FadeTo (float aValue, float aTime)
+	{
+		float alpha = _SpriteRenderer.material.color.a;
+
+		for (float t = 0.0f; t < 1.0f; t += Time.deltaTime / aTime)
+		{
+			Color newColor = new Color(1, 1, 1, Mathf.Lerp (alpha,aValue,t));
+			_SpriteRenderer.material.color = newColor;
+			yield return null;
+		}
+	}
+
+	private void OnDisable ()
+	{
+		ResetOpacity ();
+	}
+
+	private void ResetOpacity ()
+	{
+		_SpriteRenderer.material.color = new Color (
+			_SpriteRenderer.material.color.r,
+			_SpriteRenderer.material.color.g,
+			_SpriteRenderer.material.color.b,
+			0.0f
+		);
 	}
 
 	private void Update ()
 	{
-		Move();
-		CheckPosition();
+		Move ();
+		CheckPosition ();
 	}
 
 	private void Move ()
@@ -42,14 +84,14 @@ public class BouncePad : MonoBehaviour
 
 	private void CheckPosition ()
 	{
-		if(_Transform.position.y < _CullBoundary)
-			Cull();
+		if(_Transform.position.y < (_Player.position.y - _CullBoundary))
+			Cull ();
 	}
 
 	private void OnTriggerEnter2D (Collider2D other)
 	{
-		if(other.gameObject.CompareTag("Player"))
-			Cull();
+		if(other.gameObject.CompareTag ("Player"))
+			Cull ();
 	}
 
 	private void Cull ()
