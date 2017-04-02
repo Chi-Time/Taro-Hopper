4db4054461164e22623a55e2602f5a1a21c010d5
James Johnson <music1384@yahoo.co.uk>
2017-04-02 11:31:22 +0100

    Added menu controller for user interface

---
+++ Assets/Code/Classes/User Interface/UIMenuController.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class UIMenuController : MonoBehaviour
+{
+    [SerializeField] private GameObject _MenuPanel = null;
+    [SerializeField] private GameObject _CreditsPanel = null;
+
+    private bool _IsInCredit = false;
+
+    public void StartGame ()
+    {
+        //TODO: Add in state switch to game scene.
+    }
+
+    public void Credits ()
+    {
+        _IsInCredit = !_IsInCredit;
+
+        if(!_IsInCredit)
+        {
+            _MenuPanel.SetActive(false);
+            _CreditsPanel.SetActive(true);
+        }
+        else
+        {
+            _MenuPanel.SetActive(true);
+            _CreditsPanel.SetActive(false);
+        }
+    }
+
+    public void EndGame ()
+    {
+        Application.Quit ();
+    }
+
+    public void MuteMusic ()
+    {
+        //TODO: Connect with audio player and mute music.
+    }
+
+    public void MuteSFX ()
+    {
+        //TODO: Connect with audio players and mute music.
+    }
+}
