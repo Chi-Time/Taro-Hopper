c9e2fe503bc141ccbc56a528d29628aa691d142c
James Johnson <music1384@yahoo.co.uk>
2017-04-02 11:31:47 +0100

    Added UI class for game over screen.

---
+++ Assets/Code/Classes/User Interface/UIGameOverController.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class UIGameOverController : MonoBehaviour
+{
+    public void RestartGame ()
+    {
+        //TODO: switch game states.
+    }
+
+    public void ReturnToMenu ()
+    {   
+        //TODO: switch game states.
+    }
+}
