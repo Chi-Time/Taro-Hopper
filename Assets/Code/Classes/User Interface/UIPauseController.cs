e05c820283420ed1a8f82a4064e23c283ef5df30
James Johnson <music1384@yahoo.co.uk>
2017-04-02 11:31:57 +0100

    Added UI class for pause screen.

---
+++ Assets/Code/Classes/User Interface/UIPauseController.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIPauseController : MonoBehaviour
+{
+    public void MuteMusic ()
+    {
+        //TODO: Connect with audio player and mute music.
+    }
+
+    public void MuteSFX ()
+    {
+        //TODO: Connect with audio players and mute music.
+    }
+
+    public void ReturnToMenu ()
+    {
+        //TODO: Switch game state.
+    }
+}
