497b3b2068e04f35126e8a22225eada03b598726
James Johnson <music1384@yahoo.co.uk>
2017-04-05 04:36:07 +0100

    Added end score label and print method.

--- Assets/Code/Classes/User Interface/UIGameOverController.cs
+++ Assets/Code/Classes/User Interface/UIGameOverController.cs
@@ -1,11 +1,20 @@
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class UIGameOverController : MonoBehaviour
 {
+    [SerializeField] private Text _EndScoreLabel = null;
+
+    public void UpdateFinalScoreLabel (int score)
+    {
+        _EndScoreLabel.text = "Final score is: " + score.ToString ();
+    }
+
     public void RestartGame ()
     {
         //TODO: switch game states.
-        EventManager.ChangeState (GameState.Restart);
+        SceneManager.LoadScene (0);
     }
 
     public void ReturnToMenu ()
