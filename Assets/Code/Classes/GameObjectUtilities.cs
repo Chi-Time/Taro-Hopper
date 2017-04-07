107c47588029d594bd9889c6e412b6dd63be356a
James Johnson <music1384@yahoo.co.uk>
2017-04-07 13:07:31 +0100

    Added utility class to help with gameobject's
    Added static method to help grab a component from any child object
    whether active or inactive.

---
+++ Assets/Code/Classes/GameObjectUtilities.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public static class GameObjectUtilities
+{
+    public static TType GetChildComponent<TType> (Transform rootTransform) where TType : Component
+    {
+        // If we don't find the component in this object
+        // recursively iterate it's children until we do.
+        TType returnComponent = rootTransform.gameObject.GetComponent<TType> ();
+
+        if (null == returnComponent)
+        {
+            // Store the current number of children on this object.
+            var childCount = rootTransform.childCount;
+
+            // For loop is used due to foreach() mono issues.
+            for (int child = 0; child < childCount; ++child)
+            {
+                // Recusively call this method on each child.
+                // Break from the loop and return the component
+                // when it is found.
+                returnComponent = GetChildComponent<TType> (rootTransform.GetChild (child).transform);
+
+                if (null != returnComponent)
+                    break;
+            }
+        }
+
+        return returnComponent;
+    }
+}
