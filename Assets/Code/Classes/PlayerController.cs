8c5c7b096f424abf9c2333094a9bf4f2e40daded
James Johnson <music1384@yahoo.co.uk>
2017-04-13 03:53:46 +0100

    Formatted class.

--- Assets/Code/Classes/PlayerController.cs
+++ Assets/Code/Classes/PlayerController.cs
@@ -2,65 +2,68 @@
 
 public class PlayerController : MonoBehaviour
 {
-	[SerializeField] private float _Speed = 250.0f;
-	[SerializeField] private float _JumpHeight = 8.0f;
-	[SerializeField] private LayerMask _Ground;
+    [SerializeField]
+    private float _Speed = 250.0f;
+    [SerializeField]
+    private float _JumpHeight = 8.0f;
+    [SerializeField]
+    private LayerMask _Ground;
 
     private bool _HasJumped = false;
     private float _LastPadPos = 0.0f;
-	private Transform _Transform = null;
-	private Rigidbody2D _Rigidbody2D = null;
+    private Transform _Transform = null;
+    private Rigidbody2D _Rigidbody2D = null;
 
-	private void Awake ()
-	{
-		Initialise ();
-	}
-
-	private void Initialise ()
-	{
-		_Transform = GetComponent<Transform> ();
-		_Rigidbody2D = GetComponent<Rigidbody2D> ();
+    private void Awake ()
+    {
+        Initialise ();
     }
 
-	private void Start ()
-	{
-		SetDefaults ();
-	}
+    private void Initialise ()
+    {
+        _Transform = GetComponent<Transform> ();
+        _Rigidbody2D = GetComponent<Rigidbody2D> ();
+    }
 
-	private void SetDefaults ()
-	{
-		_Rigidbody2D.gravityScale = 1.0f;
-		_Rigidbody2D.isKinematic = false;
-		_Rigidbody2D.constraints = RigidbodyConstraints2D.FreezeRotation;
+    private void Start ()
+    {
+        SetDefaults ();
+    }
+
+    private void SetDefaults ()
+    {
+        _Rigidbody2D.gravityScale = 1.0f;
+        _Rigidbody2D.isKinematic = false;
+        _Rigidbody2D.constraints = RigidbodyConstraints2D.FreezeRotation;
 
         _LastPadPos = 0f;
-	}
+    }
 
-	private void Update ()
-	{
-		GetInput ();
-		CheckPosition ();
-	}
+    private void Update ()
+    {
+        GetInput ();
+        CheckPosition ();
+    }
 
-	private void GetInput ()
-	{
-		float x = Input.GetAxis ("Horizontal");
+    private void GetInput ()
+    {
+        float x = Input.GetAxis ("Horizontal");
 
-		MovePlayer (new Vector2 (x, 0f));
+        MovePlayer (new Vector2 (x, 0f));
 
-		if (Input.GetButtonDown ("Jump") && IsGrounded ())
+        if (Input.GetButtonDown ("Jump") && IsGrounded ())
         {
             Jump ();
             _HasJumped = true;
         }
-	}
+    }
 
-	private void MovePlayer (Vector2 dir)
-	{
-		_Rigidbody2D.velocity = new Vector2 (dir.x * _Speed * Time.deltaTime, _Rigidbody2D.velocity.y);
+    private void MovePlayer (Vector2 dir)
+    {
+        _Rigidbody2D.velocity = new Vector2 (dir.x * _Speed * Time.deltaTime, _Rigidbody2D.velocity.y);
 
         FlipPlayer (dir.x);
-	}
+    }
 
     private void FlipPlayer (float xDir)
     {
@@ -70,32 +73,32 @@ public class PlayerController : MonoBehaviour
             _Transform.localScale = new Vector3 (-1, 1, 1);
     }
 
-	private void CheckPosition ()
-	{
+    private void CheckPosition ()
+    {
         if (_Transform.position.y < _LastPadPos - 20f)
             EventManager.ChangeState (GameState.GameOver);
     }
 
     private bool IsGrounded ()
-	{
-		var endPos = new Vector3 (_Transform.position.x, _Transform.position.y - 0.5f, 0);
+    {
+        var endPos = new Vector3 (_Transform.position.x, _Transform.position.y - 0.5f, 0);
 
-		if (Physics2D.Linecast (_Transform.position, endPos, _Ground))
-			return true;
+        if (Physics2D.Linecast (_Transform.position, endPos, _Ground))
+            return true;
 
-		return false;
-	}
+        return false;
+    }
 
-	private void OnTriggerEnter2D (Collider2D other)
-	{
+    private void OnTriggerEnter2D (Collider2D other)
+    {
         // Check if player has collided with a bounce pad.
-		if (other.gameObject.CompareTag ("Respawn"))
+        if (other.gameObject.CompareTag ("Respawn"))
             Bounce (other);
 
         // Check if the player has landed on the floor again.
         if (other.CompareTag ("Finish") && _HasJumped)
             EventManager.ChangeState (GameState.GameOver);
-	}
+    }
 
     private void Bounce (Collider2D other)
     {
@@ -105,10 +108,10 @@ public class PlayerController : MonoBehaviour
         Jump ();
     }
 
-	private void Jump ()
-	{
-		_Rigidbody2D.AddForce (Vector2.up * _JumpHeight, ForceMode2D.Impulse);
-	}
+    private void Jump ()
+    {
+        _Rigidbody2D.AddForce (Vector2.up * _JumpHeight, ForceMode2D.Impulse);
+    }
 
     public void ResetPlayer ()
     {
