0fe912d647d6637223f32c780f5020231b8a02bc
James Johnson <music1384@yahoo.co.uk>
2017-04-14 06:21:41 +0100

    Refactored FPS counter class

--- Assets/Code/Classes/FPSDisplay.cs
+++ Assets/Code/Classes/FPSDisplay.cs
@@ -14,21 +14,26 @@ public class FPSDisplay : MonoBehaviour
     {
         if(Time.timeScale > 0.0f)
         {
-            int width = Screen.width, height = Screen.height;
-
-            var style = new GUIStyle ();
-
-            var rect = new Rect (0, 0, width, height * 2 / 100);
-
-            style.alignment = TextAnchor.UpperLeft;
-            style.fontSize = height * 2 / 100;
-            style.normal.textColor = new Color (0.0f, 0.0f, 0.5f, 1.0f);
-
-            var msec = deltaTime * 1000.0f;
-            var fps = 1.0f / deltaTime;
-            var text = string.Format ("{0:0.0} ms ({1:0.} fps)", msec, fps);
-
-            GUI.Label (rect, text, style);
+            DisplayFPSCounter ();
         }
+    }
+
+    private void DisplayFPSCounter ()
+    {
+        int width = Screen.width, height = Screen.height;
+
+        var style = new GUIStyle ();
+
+        var rect = new Rect (0, 0, width, height * 2 / 100);
+
+        style.alignment = TextAnchor.UpperLeft;
+        style.fontSize = height * 2 / 100;
+        style.normal.textColor = new Color (0.0f, 0.0f, 0.5f, 1.0f);
+
+        var msec = deltaTime * 1000.0f;
+        var fps = 1.0f / deltaTime;
+        var text = string.Format ("{0:0.0} ms ({1:0.} fps)", msec, fps);
+
+        GUI.Label (rect, text, style);
     }
 }
