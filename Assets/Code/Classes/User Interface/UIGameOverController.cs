4f6df51dfaaf8af7d62a2b49eb3c407988536492
James Johnson <music1384@yahoo.co.uk>
2017-04-14 23:27:08 +0100

    Added high score label and new parameter

--- Assets/Code/Classes/User Interface/UIGameOverController.cs
+++ Assets/Code/Classes/User Interface/UIGameOverController.cs
@@ -5,10 +5,12 @@ using UnityEngine.SceneManagement;
 public class UIGameOverController : MonoBehaviour
 {
     [SerializeField] private Text _EndScoreLabel = null;
+    [SerializeField] private Text _HighScoreLabel = null;
 
-    public void UpdateFinalScoreLabel (int score)
+    public void UpdateFinalScoreLabel (int score, int highScore)
     {
         _EndScoreLabel.text = "Final score is: " + score.ToString ();
+        _HighScoreLabel.text = "High score is: " + highScore.ToString ();
     }
 
     public void RestartGame ()
